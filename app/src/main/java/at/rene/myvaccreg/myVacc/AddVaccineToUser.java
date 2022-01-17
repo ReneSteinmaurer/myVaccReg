1aa10734f4b09cfe1dd4457b288741efb38e51be
ReneSteinmaurer <55176260+ReneSteinmaurer@users.noreply.github.com>
2022-01-17 19:30:17 +0100

    search added

--- app/src/main/java/at/rene/myvaccreg/myVacc/AddVaccineToUser.java
+++ app/src/main/java/at/rene/myvaccreg/myVacc/AddVaccineToUser.java
@@ -1,9 +1,12 @@
 package at.rene.myvaccreg.myVacc;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
@@ -11,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import at.rene.myvaccreg.R;
@@ -23,6 +27,7 @@ import at.rene.myvaccreg.roomdb.VaccinationRoom;
  */
 public class AddVaccineToUser extends Fragment {
     private RecyclerView recyclerView;
+    private EditText searchText;
     private AppCompatButton displayVaccine;
     private AllVaccsAdpater allVaccsAdapter;
     private MyVaccRegDb db;
@@ -40,6 +45,25 @@ public class AddVaccineToUser extends Fragment {
         db = MyVaccRegDb.getDbInstance(getActivity().getApplicationContext());
 
         recyclerView = getActivity().findViewById(R.id.allVaccsRecyclerView);
+        searchText = getActivity().findViewById(R.id.editTextSearch);
+
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+            }
+        });
+
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         allVaccs = db.vaccinationDao().getAllVaccines();
@@ -49,5 +73,23 @@ public class AddVaccineToUser extends Fragment {
 
 
         // Buttons und Views hinzufügen
+    }
+
+    /**
+     * Übernimmt die Funktion der Suche, der Algorithmus schaut ob die namen übereinstimmen,
+     * wenn ja, stellt die Funktion eine Liste aus der Infrage Stehenden Impfungen zusammen.
+     * Diese Liste wird dem Adapter mitgegeben.
+     * @param text
+     */
+    private void filter(String text) {
+        List<VaccinationRoom> filteredList = new ArrayList<>();
+
+        for (VaccinationRoom v : allVaccs) {
+            if (v.getName().toLowerCase().contains(text.toLowerCase())) {
+                filteredList.add(v);
+            }
+        }
+
+        allVaccsAdapter.filteredList(filteredList);
     }
 }
