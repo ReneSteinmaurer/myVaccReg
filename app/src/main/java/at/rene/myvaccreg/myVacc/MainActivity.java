1e8b0899c2a801fa291b644768f0a92fa79f2603
ReneSteinmaurer <55176260+ReneSteinmaurer@users.noreply.github.com>
2022-01-19 12:29:14 +0100

    test data ready

--- app/src/main/java/at/rene/myvaccreg/myVacc/MainActivity.java
+++ app/src/main/java/at/rene/myvaccreg/myVacc/MainActivity.java
@@ -12,6 +12,7 @@ import at.rene.myvaccreg.addVacc.AddNewVaccination;
 import at.rene.myvaccreg.addVirus.DisplayVirusesFragment;
 import at.rene.myvaccreg.roomdb.MyVaccRegDb;
 import at.rene.myvaccreg.roomdb.VaccinationRoom;
+import at.rene.myvaccreg.roomdb.Virus;
 
 public class MainActivity extends AppCompatActivity {
     private MainWindowFragment mainWindowFragment;
@@ -90,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Daten erfolgreich geladen!", Toast.LENGTH_SHORT).show();
 
             VaccinationRoom covid19 = new VaccinationRoom("Covid 19-Pfizer","COVID-19 ist eine meldepflichtige Infektionskrankheit. Sie wird verursacht vom Coronavirus SARS-CoV-2 und hat ein breites, unspezifisches Symptomspektrum.",
-                    " ", "6-12-2022", "SARS-CoV-2","Biontech Pfizer");
+                    " ", "6-12-2022", "SARS-CoV-2","Biontech");
 
             VaccinationRoom covid191 = new VaccinationRoom("Covid 19-Moderna","COVID-19 ist eine meldepflichtige Infektionskrankheit. Sie wird verursacht vom Coronavirus SARS-CoV-2 und hat ein breites, unspezifisches Symptomspektrum.",
                     " ", "6-12-2022", "SARS-CoV-2","Moderna");
@@ -104,11 +105,53 @@ public class MainActivity extends AppCompatActivity {
             VaccinationRoom ebola = new VaccinationRoom("rVSV-ZEBOV","Die Ebolaviren verursachen das Ebolafieber. Neben dem Menschen infizieren sie andere Primaten und lösen bei ihnen ein hämorrhagisches Fieber aus.",
                     " ", "9-8-2026", "Ebola","Bavarian Nordic");
 
+            VaccinationRoom herpesV = new VaccinationRoom("Herpes zoster","Herpes zoster (Gürtelrose) ist eine Viruserkrankung, die bei Menschen auftritt, die zuvor an Windpocken erkrankt sind. Das Windpocken-Virus (Varicella-zoster-Virus) kann im Körper jahrelang überleben. Wird es reaktiviert, entsteht eine Gürtelrose",
+                    " ", "21-5-2034", "Herpesviridae","Biontech");
+
+            VaccinationRoom influenzaV = new VaccinationRoom("Influenza Spaltimpfstoff","Die Ständige Impfkommission (STIKO) empfiehlt die jährliche Impfung gegen Grippe für alle, die ein erhöhtes Risiko haben, besonders schwer zu erkranken. Hierzu gehören Menschen ab 60 Jahre, chronisch Kranke jeden Alters, Schwangere sowie Bewohner von Alten- und Pflegeheimen.",
+                    " ", "10-9-2023", "Saisionale Grippe","Bavarian Nordic");
+
+            VaccinationRoom masernV = new VaccinationRoom("Masern Impfung","Die Maser-Mumps-Röteln-Impfung ist ein Lebendimpfstoff. Nach zweimaliger Impfung besteht eine lang anhaltende Immunität. ",
+                    " ", "3-4-2030", "Masern","A.C.A. Müller ADAG Pharma AG");
+
+
+
+            Virus covid = new Virus("Covid 19", "COVID-19 ist eine meldepflichtige Infektionskrankheit. Sie wird verursacht vom Coronavirus SARS-CoV-2 und hat ein breites, unspezifisches Symptomspektrum.",
+                    "SARS-CoV");
+
+            Virus herpes = new Virus("Herpes-simplex",
+                    "Die Herpes-simplex-Viren verursachen sehr verschiedene Erkrankungen des Herpes simplex," +
+                            " darunter die sogenannte Mundfäule bei Kleinkindern. Die häufigsten Symptome einer" +
+                            " Infektion dieser Viren sind jedoch Lippenherpes (Herpes labialis) und Genitalherpes (Herpes genitalis). ",
+                    "Herpesviridae");
+
+            Virus influenza = new Virus("Saisionale Grippe", "Klinisch ist ein plötzlicher Beginn " +
+                    "mit hohem Fieber und starkem subjektiven Krankheitsgefühl bei nur gering erhöhten Entzündungszeichen typisch. " +
+                    "Hohe Entzündungswerte und produktiver Husten können Hinweise auf eine bakterielle" +
+                    " Superinfektion sein (meist durch Staphylococcus aureus). Therapeutisch stehen mit den Neuraminidase-Hemmern Oseltamivir und Zanamivir zwei Medikamente zur kausalen Behandlung der Influenza zur Verfügung. ",
+                    "Influenza");
+
+            Virus hiv = new Virus("HIV", "Das Humane Immundefizienz-Virus, zumeist abgekürzt als HIV oder auch bezeichnet als Menschliches Immunschwäche-Virus oder Menschliches Immundefekt-Virus, ist ein behülltes Virus, das zur Familie der Retroviren und zur Gattung der Lentiviren gehört. ",
+                    "Retroviren");
+
+            Virus masern = new Virus("Masern", "Masern werden durch Viren ausgelöst und kommen weltweit vor. Sie sind hoch ansteckend. Eine Masern-Infektion ist keine harmlose Krankheit, denn bei etwa jedem zehnten Betroffenen treten Komplikationen auf. In Deutschland ist die Häufigkeit von Masern-Erkrankungen durch Impfungen stark zurückgegangen. Trotzdem kommt es immer wieder zu Häufungen von Krankheitsfällen bei ungeschützten Personen. ",
+                    "Paramyxoviren");
+
+
             db.vaccinationDao().insertVaccine(covid19);
             db.vaccinationDao().insertVaccine(covid191);
             db.vaccinationDao().insertVaccine(covid192);
             db.vaccinationDao().insertVaccine(covid193);
             db.vaccinationDao().insertVaccine(ebola);
+            db.vaccinationDao().insertVaccine(herpesV);
+            db.vaccinationDao().insertVaccine(influenzaV);
+            db.vaccinationDao().insertVaccine(masernV);
+
+            db.virusDao().insertVirus(covid);
+            db.virusDao().insertVirus(herpes);
+            db.virusDao().insertVirus(influenza);
+            db.virusDao().insertVirus(hiv);
+            db.virusDao().insertVirus(masern);
         } else Toast.makeText(this, "Laden fehlgeschlagen, bereits vorhanden", Toast.LENGTH_SHORT).show();
 
 
